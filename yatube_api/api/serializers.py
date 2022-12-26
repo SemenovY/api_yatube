f7c212a8e033f0613a67ec1e2bcfeb29bb163cab
SemenovY <info@juriys.ru>
2022-12-26 15:10:46 +0300

    8 failed

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -20,6 +20,7 @@ class GroupSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Comment."""
+    author = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Comment
         fields = ('id', 'text', 'author', 'post', 'created')

1ad8a2033ee670c0cc817e1dd50ad286ed9ba6f1
SemenovY <info@juriys.ru>
2022-12-26 15:33:07 +0300

    6 failed

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -36,3 +36,10 @@ class CommentViewSet(viewsets.ModelViewSet):
         post = get_object_or_404(Post, id=post_id)
         new_queryset = post.comments.all()
         return new_queryset
+
+    def perform_create(self, serializer):
+        """."""
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        serializer.save(author=self.request.user, post=post)
+

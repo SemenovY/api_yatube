19465b5a2938b55ac8a8e3122116250e6c9cd909
SemenovY <info@juriys.ru>
2022-12-27 23:08:28 +0300

    review send2, pytest ok

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework.exceptions import PermissionDenied
 
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-from posts.models import Comment, Group, Post
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
